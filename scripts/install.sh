7941744a31c5b7040a6825a05679ba36cd2049a5
Gabriel Moreno <gantoreno@gmail.com>
2021-01-11 19:03:20 -0400

    Add reminder to set saturn as ZSH_THEME after installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ git clone $HOME/projects/saturn-prompt
 
 ln -sf saturn-prompt/saturn.zsh saturn.zsh-theme
 
+echo '👽 Remember to set ZSH_THEME="saturn" in your .zshrc'
 echo '🚀 Saturn is now installed, enjoy!'
 
 source $HOME/.zshrc
