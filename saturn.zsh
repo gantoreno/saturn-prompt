c67b0f62433ca23f9a05784c6b73de62f6ac0c18
Gabriel Moreno <gantoreno@gmail.com>
2021-01-16 15:02:29 -0400

    Organize & comment sections on saturn.zsh

--- saturn.zsh
+++ saturn.zsh
@@ -7,57 +7,80 @@
 # Saturn's current version
 export SATURN_VERSION="0.1.2"
 
-# Prompt variables
+# +-----------------------------------------------------------+
+# | PROMPT VARIABLES                                          |
+# | Dynamic values that can be configured/overriden in .zshrc |
+# +-----------------------------------------------------------+
+
+# Prompt icon
 SATURN_PROMPT_ICON="${SATURN_PROMPT_ICON="🪐"}"
 
+# Path
 SATURN_PATH_COLOR="${SATURN_PATH_COLOR="cyan"}"
 
-SATURN_RIGHT_PROMPT="${SATURN_RIGHT_PROMPT="true"}"
-
+# Time
 SATURN_TIME_COLOR="${SATURN_TIME_COLOR=yellow}"
 SATURN_TIME_FORMAT="${SATURN_TIME_FORMAT="%D{%L:%M %p}"}"
 
+# Error/success status
 SATURN_SUCCESS_COLOR="${SATURN_SUCCESS_COLOR="green"}"
 SATURN_SUCCESS_ICON="${SATURN_SUCCESS_ICON="✔"}"
-
 SATURN_ERROR_COLOR="${SATURN_ERROR_COLOR="red"}"
 SATURN_ERROR_ICON="${SATURN_ERROR_ICON="✘"}"
 
+# Git prompt
 SATURN_GIT_PROMPT_SYMBOL="${SATURN_GIT_PROMPT_SYMBOL="שׂ"}"
-
 SATURN_GIT_PROMPT_COLOR="${SATURN_GIT_PROMPT_COLOR="magenta"}"
 SATURN_GIT_PROMPT_COLOR_DIRTY="${SATURN_GIT_PROMPT_COLOR_DIRTY="red"}"
 
-if [[ "$plugins" =~ "shrink-path" ]] then
-  SATURN_PATH_FORMAT="${SATURN_PATH_FORMAT=$(shrink_path -f)}"
-fi
-
-# Prompt (left)
-PROMPT=$''
-PROMPT+=$'$SATURN_PROMPT_ICON '                             # Prompt icon
-
-if [[ "$plugins" =~ "shrink-path" ]] then
-  PROMPT+=$'%B%F{$SATURN_PATH_COLOR}$(shrink_path -f)%f%b ' # Use shrink_path if present
-else
-  PROMPT+=$'%B%F{$SATURN_PATH_COLOR}%~%f%b '                # Fallback to regular path
-fi
-
-PROMPT+=$'$(git_prompt_info)'                               # VCS
-
-if [[ "$TERM_PROGRAM" == "vscode" ]] then
-  PROMPT+=$' '                                              # Add extra space for VSCode
-fi
-
-# Prompt (right)
-RPROMPT=$''
-
-if [[ $SATURN_RIGHT_PROMPT == "true" ]] then
-  RPROMPT+=$'%(?.%F{$SATURN_SUCCESS_COLOR}$SATURN_SUCCESS_ICON.%F{$SATURN_ERROR_COLOR}$SATURN_ERROR_ICON%f) ' # Status
-  RPROMPT+=$'%B%F{$SATURN_TIME_COLOR}$SATURN_TIME_FORMAT%f%b'                                                 # Time
-fi
+# Right prompt
+SATURN_RIGHT_PROMPT="${SATURN_RIGHT_PROMPT="true"}"
 
 # Git elements
 ZSH_THEME_GIT_PROMPT_PREFIX="on %B%F{$SATURN_GIT_PROMPT_COLOR}"
 ZSH_THEME_GIT_PROMPT_SUFFIX=" %F{$SATURN_GIT_PROMPT_COLOR}$SATURN_GIT_PROMPT_SYMBOL%f%b "
 ZSH_THEME_GIT_PROMPT_DIRTY=" %F{$SATURN_GIT_PROMPT_COLOR_DIRTY}[!]%f"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+if [[ "$plugins" =~ "shrink-path" ]] then
+  SATURN_PATH_FORMAT="${SATURN_PATH_FORMAT=$(shrink_path -f)}"
+fi
+
+# +------------------------------------------------+
+# | PROMPT                                         |
+# | Build the prompt based on the config variables |
+# +------------------------------------------------+
+
+# Prompt
+PROMPT=$''
+
+# Prompt icon
+PROMPT+=$'$SATURN_PROMPT_ICON '
+
+# Use shrink_path if present
+# Otherwise, fallback to regular path
+if [[ "$plugins" =~ "shrink-path" ]] then
+  PROMPT+=$'%B%F{$SATURN_PATH_COLOR}$(shrink_path -f)%f%b '
+else
+  PROMPT+=$'%B%F{$SATURN_PATH_COLOR}%~%f%b '
+fi
+
+# VCS
+PROMPT+=$'$(git_prompt_info)'
+
+# Extra space on VSCode
+if [[ "$TERM_PROGRAM" == "vscode" ]] then
+  PROMPT+=$' '
+fi
+
+# Check if RPROMPT is present
+if [[ $SATURN_RIGHT_PROMPT == "true" ]] then
+  # RPROMPT
+  RPROMPT=$''
+
+  # Error/success status
+  RPROMPT+=$'%(?.%F{$SATURN_SUCCESS_COLOR}$SATURN_SUCCESS_ICON.%F{$SATURN_ERROR_COLOR}$SATURN_ERROR_ICON%f) '
+
+  # Time
+  RPROMPT+=$'%B%F{$SATURN_TIME_COLOR}$SATURN_TIME_FORMAT%f%b'
+fi
